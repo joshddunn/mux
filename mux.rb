200bd0290521839e07654e5217a8a9b91fb7beb0
Josh Dunn <joshdunn.uwo@gmail.com>
2025-02-09 00:26:59 -0500

    update version, sha256, and download link

--- mux.rb
+++ mux.rb
@@ -1,10 +1,10 @@
 # https://docs.brew.sh/Cask-Cookbook
 
 cask "mux" do
-  version "0.0.1"
-  sha256 "todo"
+  version "v0.1.0"
+  sha256 "c73b2815494508d75987b310305072ab3996ce138fbe37ad0860373998d9f8c1"
 
-  url "https://github.com/joshddun/mux/archive/#{version}.tar.gz"
+  url "https://github.com/joshddunn/mux/archive/refs/tags/#{version}.tar.gz"
   name "mux"
   desc "Configure tmux sessions"
   homepage "https://github.com/joshddunn/mux"
