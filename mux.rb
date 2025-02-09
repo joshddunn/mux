930f938bc0089624536c38df40c7418b1721b4b2
Josh Dunn <joshdunn.uwo@gmail.com>
2025-02-09 00:46:04 -0500

    homebrew mux

--- mux.rb
+++ mux.rb
@@ -4,10 +4,10 @@ cask "mux" do
   version "v0.1.0"
   sha256 "c73b2815494508d75987b310305072ab3996ce138fbe37ad0860373998d9f8c1"
 
-  url "https://github.com/joshddunn/mux/archive/refs/tags/#{version}.tar.gz"
+  url "https://github.com/joshddunn/homebrew-mux/archive/refs/tags/#{version}.tar.gz"
   name "mux"
   desc "Configure tmux sessions"
-  homepage "https://github.com/joshddunn/mux"
+  homepage "https://github.com/joshddunn/homebrew-mux"
 
   binary "mux"
 end
