8fa33b7018a46293ee041a3690793ade01c4124e
Josh Dunn <joshdunn.uwo@gmail.com>
2025-02-14 16:55:53 -0500

    add help command

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ brew install joshddunn/tap/mux
 | command           | description                                     |
 | ----------------- | ----------------------------------------------- |
 | config            | Open config file (`~/.mux.json`) with `$EDITOR` |
+| help              | Link to GitHub homepage                         |
 | start \<session\> | Start session in config file                    |
 | stop \<session\>  | Stop session in config file                     |
 
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"mux/lib/builder"
 	"mux/lib/config"
@@ -8,18 +9,32 @@ import (
 )
 
 const (
-	Config = "config"
-	Start  = "start"
-	Stop   = "stop"
+	Config  = "config"
+	Help    = "help"
+	Start   = "start"
+	Stop    = "stop"
+	Version = "v0.1.3"
 )
 
 func main() {
 	log.SetFlags(0)
-	args := os.Args[1:]
+	cmd := os.Args[1]
+	args := os.Args[2:]
 
-	switch args[0] {
+	versionPtr := flag.Bool("version", false, "mux version")
+
+	flag.Parse()
+
+	if *versionPtr {
+		log.Print(Version)
+		os.Exit(0)
+	}
+
+	switch cmd {
 	case Config:
 		config.EditConfig()
+	case Help:
+		log.Print("https://github.com/joshddunn/mux")
 	case Start:
 		startSession(args)
 	case Stop:
@@ -30,17 +45,17 @@ func main() {
 }
 
 func startSession(args []string) {
-	if len(args) != 2 {
+	if len(args) != 1 {
 		log.Fatal("Invalid command line args")
 	}
 
-	builder.StartSession(args[1])
+	builder.StartSession(args[0])
 }
 
 func stopSession(args []string) {
-	if len(args) != 2 {
+	if len(args) != 1 {
 		log.Fatal("Invalid command line args")
 	}
 
-	builder.StopSession(args[1])
+	builder.StopSession(args[0])
 }
